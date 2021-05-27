4aab1b607a5d98843e22fed566dd456d3cbf6859
mallyapravin@gmail.com <mallyapravin@gmail.com>
2021-05-27 12:26:26 +0530

    Successful commit

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const route = require('./Routes/routes')
 const app = express();
+const cors = require('cors')
 
 app.use(bodyparser.json());
+app.use(cors());
 app.use('/mCart', route);
 
 const port = process.env.PORT || 8000;
